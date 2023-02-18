5517b4ade0cbee81eb778e175280d11e97942b83
J2mF <mp4-12cs5@hotmail.com>
2023-02-18 18:50:56 +0300

    fix 🔧: fix create renderer func signature

--- celeritas.go
+++ celeritas.go
@@ -113,7 +113,7 @@ func (c *Celeritas) CheckDotEnv(path string) error {
 		renderer: os.Getenv("RENDERER"),
 	}
 
-	c.Render = c.createRenderer(c)
+	c.createRenderer()
 
 	return nil
 }
@@ -128,12 +128,12 @@ func (c *Celeritas) startLoggers() (*log.Logger, *log.Logger) {
 	return infoLog, errorLog
 }
 
-func (c *Celeritas) createRenderer(cel *Celeritas) *render.Render {
+func (c *Celeritas) createRenderer() {
 	myRenderer := render.Render{
-		Renderer: cel.config.renderer,
-		RootPath: cel.RootPath,
-		Port:     cel.config.port,
+		Renderer: c.config.renderer,
+		RootPath: c.RootPath,
+		Port:     c.config.port,
 	}
 
-	return &myRenderer
+	c.Render = &myRenderer
 }
