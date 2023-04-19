9df1359c599b0a5d9235ad808b36f20248737386
J2mF <mp4-12cs5@hotmail.com>
2023-04-19 14:49:17 +0300

    feat: add Log for session middleware

--- middleware.go
+++ middleware.go
@@ -3,5 +3,6 @@ package celeritas
 import "net/http"
 
 func (c *Celeritas) SessionLoad(next http.Handler) http.Handler {
+	c.InfoLog.Println("SessionLoad called")
 	return c.Session.LoadAndSave(next)
 }
