2202f258e866222c61982f0604d94d37589e9b38
J2mF <mp4-12cs5@hotmail.com>
2023-08-30 20:13:23 +0300

    feat: implement random string function

--- helpers.go
+++ helpers.go
@@ -1,6 +1,25 @@
 package celeritas
 
-import "os"
+import (
+	"crypto/rand"
+	"os"
+)
+
+const (
+	randomString = "abcdefghijklmnopqrstuvwxyzABCDEFGIJKLMNOPQRSTUVWXYZ0987654321_+"
+)
+
+func (c *Celeritas) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomString)
+
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+
+	return string(s)
+}
 
 func (c *Celeritas) CreateDirIfNotExist(path string) error {
 	const mode = 0755
