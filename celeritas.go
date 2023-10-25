5fbb52ec94ff3d5ee7aef156674c02b87d2305c4
J2mF <mp4-12cs5@hotmail.com>
2023-10-25 16:50:55 +0300

    feat: add default session

--- celeritas.go
+++ celeritas.go
@@ -188,6 +188,7 @@ func (c *Celeritas) createRenderer() {
 		RootPath: c.RootPath,
 		Port:     c.config.port,
 		JetViews: c.JetViews,
+		Session:  c.Session,
 	}
 
 	c.Render = &myRenderer
--- render/render.go
+++ render/render.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/CloudyKit/jet/v6"
+	"github.com/alexedwards/scs/v2"
 	"html/template"
 	"log"
 	"net/http"
@@ -17,6 +18,7 @@ type Render struct {
 	Port       string
 	ServerName string
 	JetViews   *jet.Set
+	Session    *scs.SessionManager
 }
 
 type TemplateData struct {
@@ -29,6 +31,16 @@ type TemplateData struct {
 	Port            string
 	ServerName      string
 	Secure          bool
+}
+
+func (c *Render) defaultData(td *TemplateData, r *http.Request) *TemplateData {
+	td.Secure = c.Secure
+	td.ServerName = c.ServerName
+	td.Port = c.Port
+	if c.Session.Exists(r.Context(), "userID") {
+		td.IsAuthenticated = true
+	}
+	return td
 }
 
 func (c *Render) Page(w http.ResponseWriter, r *http.Request, view string, variables, data interface{}) error {
@@ -73,6 +85,7 @@ func (c *Render) JetPage(w http.ResponseWriter, r *http.Request, templateName st
 	}
 
 	td := &TemplateData{}
+	td = c.defaultData(td, r)
 	if data != nil {
 		td = data.(*TemplateData)
 	}
